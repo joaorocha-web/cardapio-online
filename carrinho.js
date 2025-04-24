32864379aa4a29d74ac2f13a3485b79daded2c39
joaorocha-web <jvrochajf@eaportal.org>
2025-04-24 17:40:50 -0300

    adicionando pedidos ao carrinho

--- carrinho.js
+++ carrinho.js
@@ -1,9 +1,9 @@
 const carrinho = document.getElementsByClassName('carrinho-formato')[0];
-const botaoCarrinho = document.getElementById('pedidos');
+const botaoCarrinho = document.getElementById('botao-carrinho');
 const botaoFechar = document.getElementById('fechar');
 
 // Abrir Pedidos
-botaoCarrinho.addEventListener('click', function abrirPedidos(){
+botaoCarrinho.addEventListener('click', function(){
     carrinho.style.display= 'flex'
 })
 
@@ -11,3 +11,24 @@ botaoCarrinho.addEventListener('click', function abrirPedidos(){
 botaoFechar.addEventListener('click', function fecharPedidos(){
     carrinho.style.display= 'none'
 })
+
+// Contar pedidos 
+const pedidos = document.getElementById('pedidos')
+const mostrarTotal = document.getElementById('total')
+let contador = document.getElementById('cont-pedidos')
+let cont = 0
+let total = 0
+document.querySelectorAll('button.comprar').forEach (botao =>{
+    botao.addEventListener('click', function (){
+        let nome = botao.getAttribute('data-name')
+        let preco = botao.getAttribute('data-price')
+        pedidos.innerHTML += `${nome} - ${preco} <br>`
+        total += Number(preco)
+        mostrarTotal.innerHTML = `<strong>R$${total},00</strong>`
+        cont++
+        contador.innerHTML =`${cont}`
+    })
+})
+
+// Adicionar Pedidos no Carrinho
+
