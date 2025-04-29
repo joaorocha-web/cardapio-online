5cbd9743d6a99e7f32ac9d14d78daababfc40519
joaorocha-web <jvrochajf@eaportal.org>
2025-04-29 14:24:54 -0300

    adicionando imagens com interação js

--- selecionar.js
+++ selecionar.js
@@ -1,18 +1,21 @@
+const imgPizza = document.getElementById('img-pizza')
+
+
+
 const desabilitar1 = document.getElementById('desabilitado1')
 const desabilitar2 = document.getElementById('desabilitado2')
 const estiloDesabilitar2 = window.getComputedStyle(desabilitar2).display;
-document.querySelectorAll('.opcao').forEach(sabor =>{
-    sabor.addEventListener('click', function(){
-        let nome = sabor.getAttribute('data-name');
-        let preco = Number(sabor.getAttribute('data-price'));
-       
-
+document.querySelectorAll('.opcao').forEach(saborPizza =>{
+    saborPizza.addEventListener('click', function(){
+        let nome = saborPizza.getAttribute('data-name');
+        let preco = Number(saborPizza.getAttribute('data-price'));
+        let sabor = saborPizza.getAttribute('data-sabor')
+        imgPizza.src = `pizza-metade-${sabor}.png`
         
-    desabilitar1.style.display = 'flex';
 
-
-    const estiloAtual = window.getComputedStyle(desabilitar2).display;
-    desabilitar2.style.display = estiloAtual === 'flex' ? 'none' : 'flex';
+        desabilitar1.style.display= 'flex'
+        const estiloAtual = window.getComputedStyle(desabilitar2).display;
+        desabilitar2.style.display = estiloAtual === 'flex' ? 'none' : 'flex';
     })
 
    
